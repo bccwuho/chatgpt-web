f4b56a1b09a20baabe8064ac89bdc17ea5d5f4fc
bccwuho <joewu72@139.com>
2023-03-13 08:31:58 +0800

    test1

--- src/components/custom/GithubSite.vue
+++ src/components/custom/GithubSite.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="text-neutral-400">
-    <span>Star on</span>
+    <span>Star on1</span>
     <a href="https://github.com/Chanzhaoyu/chatgpt-bot" target="_blank" class="text-blue-500">
       GitHub
     </a>
